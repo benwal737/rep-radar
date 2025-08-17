f5676fca5b9f6ffbfa392949ef26c83c7305b362
Ben Walderman <benlwalderman@gmail.com>
2025-08-16 18:57:58 -0700

    clerk

---
+++ src/app/login/Login.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const Login = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
